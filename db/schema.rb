935fbcd00eecc8d62f7ca222b23fdce4f5748a15
Cecilia Cagnetta <mariaceciliacagnetta@gmail.com>
2023-11-17 10:30:25 +0000

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 0) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension 'plpgsql'
+
+  create_table 'bookmarks', force: :cascade do |t|
+    t.string 'comment'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.bigint 'list_id', null: false
+    t.bigint 'movie_id', null: false
+    t.index ['list_id'], name: 'index_bookmarks_on_list_id'
+    t.index ['movie_id'], name: 'index_bookmarks_on_movie_id'
+  end
+
+  create_table 'lists', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
+
+  create_table 'movies', force: :cascade do |t|
+    t.string 'title'
+    t.text 'overview'
+    t.text 'poster_url'
+    t.integer 'rating'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
+
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'bookmarks', 'lists'
+  add_foreign_key 'bookmarks', 'movies'
 end
